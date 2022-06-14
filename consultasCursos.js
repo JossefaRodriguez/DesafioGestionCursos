389c10e181b8b13f7e806fad6f6d65ae3c3ef794
Jossefa <javierarodriguezvera@gmail.com>
2022-06-13 22:25:36 -0400

    Mejoras

--- consultasCursos.js
+++ consultasCursos.js
@@ -1,4 +1,3 @@
-const res = require('express/lib/response')
 const { Pool } = require('pg')
 const pool = new Pool({
     user: 'postgres',
@@ -10,7 +9,11 @@ const pool = new Pool({
 
 async function nuevoCurso(curso) {
     try {
-        const result = await pool.query(`INSERT INTO cursos (nombre, nivel, fecha, duracionDias) VALUES ('${curso.nombre}', ${curso.nivelTecnico}, '${curso.fechaInicio}', ${curso.duracion}) RETURNING *`)
+        const sqlQuery = {
+            text: 'INSERT INTO cursos (nombre, nivel, fecha, duracionDias) VALUES ($1, $2, $3, $4) RETURNING *',
+            values: [curso.nombre, curso.nivelTecnico, curso.fechaInicio, curso.duracion]
+        }
+        const result = await pool.query(sqlQuery)
         return result.rows
 
     } catch (error) {
@@ -20,7 +23,10 @@ async function nuevoCurso(curso) {
 
 async function obtenerCursos() {
     try {
-        const result = await pool.query("SELECT id, nombre, nivel, to_char(fecha,'YYYY-MM-DD') AS fecha, duraciondias FROM cursos")
+        const sqlquery = {
+            text: "SELECT id, nombre, nivel, to_char(fecha,'yyyy-mm-dd') AS fecha, duraciondias FROM cursos"
+        }
+        const result = await pool.query(sqlquery)
         return result.rows
     } catch (error) {
         return error
@@ -29,23 +35,31 @@ async function obtenerCursos() {
 
 async function editarCurso(id, curso) {
     try {
-        const result = await pool.query(`UPDATE cursos SET nombre = '${curso.nombre}', nivel = ${curso.nivelTecnico}, fecha = '${curso.fechaInicio}', duraciondias = ${curso.duracion} WHERE id = ${id} RETURNING *`)
+        const sqlQuery = {
+            text: 'UPDATE cursos SET nombre = $1, nivel = $2, fecha = $3, duraciondias = $4 WHERE id = $5 RETURNING *',
+            values:[curso.nombre, curso.nivelTecnico, curso.fechaInicio, curso.duracion, id]
+        }
+        const result = await pool.query(sqlQuery)
         return result.rows
-
     } catch (error) {
         return error
     }
 }
 
-async function eliminarCurso (id) {
+async function eliminarCurso(id) {
     try {
-        const result = await pool.query(`DELETE FROM cursos WHERE id = ${id} RETURNING *`)
+        const sqlQuery = {
+            text: 'DELETE FROM cursos WHERE id = $1 RETURNING *',
+            values: [id]
+        }
+        const result = await pool.query(sqlQuery)
         return result.rowCount
-        
+
     } catch (error) {
         return error
     }
 }
+
 
 
 module.exports = { nuevoCurso, obtenerCursos, editarCurso, eliminarCurso }
